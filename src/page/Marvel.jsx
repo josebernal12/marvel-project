5fbf7bde6fcc1b1a1648f12a008f5627e0c40bfa
jesusDev <jesus_dioker@hotmail.com>
2023-06-17 18:47:37 -0700

    Marvel

--- src/page/Marvel.jsx
+++ src/page/Marvel.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+
 
 export const Marvel = () => {
   return (
-    <div>Marvel</div>
+    <div className="scroll">
+     <div className="container-xxl">
+     <div className="row">
+     <div className="col-3">
+      <div>
+        <h5>Personajes</h5>
+      </div>
+     </div>
+     <div className="col-3"></div>
+     <div className="col-3"></div>
+     <div className="col-3"></div>
+     </div>
+     </div>
+    </div>
   )
 }
