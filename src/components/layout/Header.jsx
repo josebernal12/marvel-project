90cfba6f983b2f8405e06fbbd058755c7499c570
jose <jose12_bernal@hotmail.com>
2023-06-18 13:01:58 -0700

    dashboard added

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,16 +1,16 @@
 export const Header = () => {
   return (
-    <div className="d-flex justify-content-around align-items-center">
+    <div className="d-flex justify-content-around align-items-center py-3">
       <p className="m-0 text-white fs-3">Name: Jose</p>
       <form className="d-flex gap-3">
-        <input type="text" className="form-control px-5 " />
+        <input type="text" className="form-control px-5  header-search fs-3" />
         <input
           type="submit"
-          className="btn btn-outline-primary px-4 text-uppercase py-3 header-search"
+          className="btn btn-outline-primary px-4 text-uppercase py-3 fs-5 "
           value="search"
         />
       </form>
-      <button type="submit" className="btn btn-outline-danger px-4 text-uppercase py-3">Logout</button>
+      <button type="submit" className="btn btn-outline-danger px-4 text-uppercase py-3 fs-5">Logout</button>
     </div>
   );
 };
